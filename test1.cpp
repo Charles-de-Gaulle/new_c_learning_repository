0ca4d9b847bc0d64bac43efa0c10c684f9c8ac9f
Charles-de-Gaulle <1918434637@qq.com>
2021-11-17 16:45:37 +0800

    old work

---
+++ test1.cpp
@@ -0,0 +1,107 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	int sum = 0;
+//	scanf("%d%d", &num1, &num2);
+//	sum = num1 + num2;
+//	printf("sum=%d", sum);
+//	return 0;
+//}
+//int main()
+//{
+//	char arr1[] = "ab\0c";
+//	char arr2[] = { 'a','b','c' ,'\0'};
+//	printf("%d\n", strlen(arr1));
+//	printf("%d\n", strlen(arr2));
+//	return 0;
+//}
+//int main()
+//{
+//	printf("%d",strlen("C:\test\32\test.c"));
+//	return 0;
+//}
+//int main()
+//{
+//	int line = 0;
+//	while (line < 20000)
+//	{
+//		printf("ÇÃ´úÂë\n");
+//		line++;
+//	}
+//	printf("good offer\n");
+//	return 0;
+//}
+//int main()
+//{
+//	int arr[10] = {  };
+//	int i = 0;
+//	while (i < 10)
+//	{
+//		printf("%c\n", arr[i]);
+//		i++;
+//	};
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 0;
+//	int b = ~a;
+//	printf("%d\n", sizeof(a));
+//	printf("%d\n", sizeof(b));
+//	return 0;
+//}
+//int main()
+//{
+//	printf("hello world");
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 1;
+//	int b = 1;
+//	int max = 0;
+//	if (a >= b)
+//		max = a;
+//	else
+//		max = b;
+//	printf("max=%d", max);
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 1;
+//	int b = 2;
+//	int max = 0;
+//	max = (a > b ? a : b);
+//	printf("max=%d", max);
+//	return 0;
+//}
+//#include<math.h>
+//int is_prime(int a)//ÖÊÊıÅĞ¶ÏÆ÷
+//{
+
+	//using namespace std;
+//		int b = sqrt(a);
+//	int m = 1;
+//	for (m = 1; m <= b; m++)
+//	{
+//		int ret = a % m;
+//		if (ret == 0 && m != 1)
+//		{
+//			goto case0;
+//		}
+//		else if (m == b)
+//		{
+//			goto case1;
+//		}
+//	}
+//case0:
+//	return 0;
+//case1:
+//	return 1;
+//
+//}
