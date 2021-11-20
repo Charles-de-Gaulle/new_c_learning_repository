e40a66e817e1928f97d7c3598606d471bc230a9f
Charles-de-Gaulle <1918434637@qq.com>
2021-11-20 10:59:10 +0800

    11.20.2021

---
+++ 11.18.2021.c
@@ -0,0 +1,16 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main()
+//{
+//	/*int i = 0;
+//	int arr[5] = { 0 };
+//	for (i = 0; i < 5; i++)
+//	{
+//		scanf("%d ", &arr[i]);
+//		printf("%d*****\n", arr[i]);
+//	}*/
+//	int arr[5] = { 0 };
+//	scanf("%d\n%d\n%d\n", &arr[0], &arr[1], &arr[2]);
+//	scanf("%d\n%d\n", &arr[3], &arr[4]);
+//	return 0;
+//}
---
+++ 11.20.2021.c
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+#include<memory.h>
+int main()
+{
+	struct member
+	{
+		char name[8];
+		int chines;
+		int math;
+		int english;
+		int sum;
+	};
+	struct member* pc = malloc(sizeof(struct member) * 1000);
+
+	
+	int i, n, max, count;
+	for (i = 0; i < 1000; i++)
+	{
+		memset(&pc[i], 0, sizeof(struct member));
+	}
+	scanf("%d", &n);
+	for (i = 0; i <n ; i++)
+	{
+		scanf("%s %d %d %d", &pc[i].name, &pc[i].chines, &pc[i].math, &pc[i].english);
+		pc[i].sum = pc[i].chines + pc[i].math + pc[i].english;
+	}
+	max = 0;
+	count = 0;
+	for (i = 0; i < n; i++)
+	{
+
+		if (max<pc[i].sum)
+		{
+			max = pc[i].sum;
+			count = i;
+			
+		}
+		else
+		{
+			;
+		}
+	}
+	printf("%s %d %d %d\n", pc[count].name, pc[count].chines, pc[count].math, pc[count].english);
+	return 0;
+}
